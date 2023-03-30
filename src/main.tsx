e55a8aa2926144f63b367a78fdf2b1a072e31a10
yordy <yordysh28@gmail.com>
2023-03-29 22:52:45 -0500

    Add base template

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+
+import '@popperjs/core';
+import 'bootstrap';
+import 'perfect-scrollbar';
+import './././template/js/custom.js';
+import './././template/js/sidebarmenu.js';
+
+import './styles/app.scss';
+
 import App from './App';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
